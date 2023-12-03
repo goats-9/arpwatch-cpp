99befa8c1cdff102b7ba82152717d8f7a8e82129
Gautam Singh <goats.singh@gmail.com>
2023-12-03 21:04:21 +0530

    Move env.hpp and add skeleton for main code

--- include/arpwatch.hpp
+++ include/arpwatch.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <signal.h>
 #include "arp_checks.hpp"
 #include "db_interface.hpp"
 #include "send_mail.hpp"
--- src/arpwatch.cpp
+++ src/arpwatch.cpp
@@ -1,5 +1,24 @@
 #include "arpwatch.hpp"
 
+void exit_handler() {
+    std::cout << "Ctrl+C detected, exiting..." << std::endl;
+    exit(0);
+}
+
 int main(int argc, char *argv[]) {
+    // Define signal handler
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = exit_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    // Main code goes here
+    while (1) {
+
+    }
+
     return 0;
 }
