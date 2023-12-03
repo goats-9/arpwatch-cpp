2e6007ef48f09a01642e1002fbe873bc49e5ca4f
Gautam Singh <goats.singh@gmail.com>
2023-12-04 03:09:24 +0530

    Change sql file

--- sql/arp.sql
+++ sql/arp.sql
@@ -1,8 +1,8 @@
 -- Create the table
-CREATE TABLE arp_records (
+CREATE OR REPLACE TABLE arp_records (
     mac VARCHAR(17) NOT NULL,
     ip VARCHAR(64),
     iface VARCHAR(50),
     tstamp INT NOT NULL,
-    PRIMARY KEY (ip_address, mac_address)
+    PRIMARY KEY (ip, mac)
 );
